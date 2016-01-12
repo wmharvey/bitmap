262b74a565498a6b7c187dd92ff8212c1cc46a03
Whitney Harvey <wmelrose01@gmail.com>
2016-01-12 15:41:14 -0800

    add testing files and lint js files

--- readFile.js
+++ readFile.js
@@ -1,39 +1,22 @@
 var fs = require('fs');
 
-function getBuffer (filename) {
+exports.getBuffer = function getBuffer (filename) {
   return fs.readFileSync(filename);
 };
 
-function getImageInfo (buffer) {
+exports.getImageInfo = function getImageInfo (buffer) {
   var image = {};
-
   image.type = String.fromCharCode(buffer.readUInt8(0)) + String.fromCharCode(buffer.readUInt8(1));
-  console.log('image type: ' + image.type);
-
   image.start = buffer.readUInt32LE(10);
-  console.log('image start:' + image.start);
-
   image.dibHeaderSize = buffer.readUInt32LE(14);
-  console.log('image headerSize:' + image.dibHeaderSize);
-
   image.width = buffer.readInt32LE(18);
-  console.log('image width: ' + image.width);
-
   image.height = buffer.readInt32LE(22);
-  console.log('image height: ' + image.height);
-
   image.depth = buffer.readUInt16LE(28);
-  console.log('image depth: ' + image.depth);
-
   image.size = buffer.readUInt32LE(34);
-  console.log('image size: ' + image.size);
 
   if (image.depth <= 8) {
     image.paletteSize = Math.pow(2, image.depth) * 4;
-    console.log('paletteSize:', image.paletteSize);
-
     image.palette = getPalette();
-    console.log('image palette:', image.palette[28].red, image.palette[28].green, image.palette[28].blue);
   }
 
   function getPalette () {
@@ -47,71 +30,75 @@ function getImageInfo (buffer) {
       paletteArr.push(color);
     }
     return paletteArr;
-  };
+  }
   return image;
 };
 
-function reverseImage (readFile, writeFile) {
-  var buffer = getBuffer(readFile);
-  var image = getImageInfo(buffer);
-
-  var revBuffer = new Buffer (buffer);
+exports.reverseImage = function reverseImage (readFile, writeFile, callback) {
+  var buffer = this.getBuffer(readFile);
+  var image = this.getImageInfo(buffer);
+  var getRow = image.palete ? getPalette : getNonPalette;
+  var writeRow = image.palette ? writePalette : writeNonPalette;
   for (var row = 0; row < image.height; row++) {
     var regArray = getRow(row);
     var revArray = regArray.reverse();
     writeRow(row);
   }
-  fs.writeFile(writeFile, revBuffer);
+  fs.writeFileSync(writeFile, buffer);
+  callback();
 
-  function getRow(row) {
+  function getPalette(row) {
     var rowArray = [];
-    if (image.palette) {
-      var rowSize = image.width;
-      for (var i = 0; i < image.width; i++) {
-        rowArray.push(buffer.readUInt8(image.start + (rowSize * row) + i));
-      }
-    } else {
-      var rowSize = image.width * 3;
-      for (var i = 0; i < rowSize; i += 3) {
-        var pixel = {};
-        pixel.blue = buffer.readUInt8(image.start + (rowSize * row) + i);
-        pixel.green = buffer.readUInt8(image.start + (rowSize * row) + i + 1);
-        pixel.red = buffer.readUInt8(image.start + (rowSize * row) + i + 2);
-        rowArray.push(pixel);
-      }
+    var rowSize = image.width;
+    for (var i = 0; i < rowSize; i++) {
+      rowArray.push(buffer.readUInt8(image.start + (rowSize * row) + i));
     }
     return rowArray;
-  };
+  }
 
-  function writeRow(row) {
-    var rowSize = image.width;
-    if (image.palette) {
-      for (var i = 0; i < rowSize; i++) {
-        revBuffer.writeUInt8(revArray[i], image.start + (rowSize * row)  + i);
-      }
-    } else {
-      rowSize *= 3;
-      index = 0;
-      for (var i = 0; i < rowSize; i += 3) {
-        revBuffer.writeUInt8(regArray[index].blue, image.start + (rowSize * row) + i);
-        revBuffer.writeUInt8(regArray[index].green, image.start + (rowSize * row) + i + 1);
-        revBuffer.writeUInt8(regArray[index].red, image.start + (rowSize * row) + i + 2);
-        index++;
-      }
+  function getNonPalette(row) {
+    var rowArray = [];
+    var rowSize = image.width * 3;
+    for (var i = 0; i < rowSize; i += 3) {
+      var pixel = {};
+      pixel.blue = buffer.readUInt8(image.start + (rowSize * row) + i);
+      pixel.green = buffer.readUInt8(image.start + (rowSize * row) + i + 1);
+      pixel.red = buffer.readUInt8(image.start + (rowSize * row) + i + 2);
+      rowArray.push(pixel);
     }
-  };
+    return rowArray;
+  }
+
+  function writePalette(row) {
+    var rowSize = image.width;
+    for (var i = 0; i < rowSize; i++) {
+      buffer.writeUInt8(revArray[i], image.start + (rowSize * row)  + i);
+    }
+  }
+
+  function writeNonPalette(row) {
+    var rowSize = image.width * 3;
+    var index = 0;
+    for (var i = 0; i < rowSize; i += 3) {
+      buffer.writeUInt8(regArray[index].blue, image.start + (rowSize * row) + i);
+      buffer.writeUInt8(regArray[index].green, image.start + (rowSize * row) + i + 1);
+      buffer.writeUInt8(regArray[index].red, image.start + (rowSize * row) + i + 2);
+      index++;
+    }
+  }
 };
 
-function invertColors(readFile, writeFile) {
-  var buffer = getBuffer(readFile);
-  var image = getImageInfo(buffer);
+exports.invertColors = function invertColors(readFile, writeFile, callback) {
+  var buffer = this.getBuffer(readFile);
+  var image = this.getImageInfo(buffer);
 
   if (image.depth <= 8) {
     invertPalette();
   } else {
     invertPixels();
   }
-  fs.writeFile(writeFile, buffer);
+  fs.writeFileSync(writeFile, buffer);
+  callback();
 
   function invertPixels() {
     for (var offset = 0; offset < 30000; offset += 3) {
@@ -122,7 +109,7 @@ function invertColors(readFile, writeFile) {
       buffer.writeUInt8(newGreen, image.start + offset + 1);
       buffer.writeUInt8(newRed, image.start + offset + 2);
     }
-  };
+  }
 
   function invertPalette() {
     var index = 0;
@@ -135,19 +122,20 @@ function invertColors(readFile, writeFile) {
       buffer.writeUInt8(newBlue, 14 + image.dibHeaderSize + offset + 2);
       index++;
     }
-  };
+  }
 };
 
-function grayScale(readFile, writeFile) {
-  var buffer = getBuffer(readFile);
-  var image = getImageInfo(buffer);
+exports.grayScale = function grayScale(readFile, writeFile, callback) {
+  var buffer = this.getBuffer(readFile);
+  var image = this.getImageInfo(buffer);
 
   if (image.depth <= 8) {
     grayscalePalette();
   } else {
     grayscalePixels();
   }
-  fs.writeFile(writeFile, buffer);
+  fs.writeFileSync(writeFile, buffer);
+  callback();
 
   function grayscalePixels () {
     for (var offset = 0; offset < 30000; offset += 3) {
@@ -159,7 +147,7 @@ function grayScale(readFile, writeFile) {
       buffer.writeUInt8(hue, image.start + offset + 1);
       buffer.writeUInt8(hue, image.start + offset + 2);
     }
-  };
+  }
 
   function grayscalePalette() {
     var index = 0;
@@ -170,10 +158,5 @@ function grayScale(readFile, writeFile) {
       buffer.writeUInt8(hue, 14 + image.dibHeaderSize + offset + 2);
       index++;
     }
-  };
+  }
 };
-
-
-// reverseImage('./img/non-palette-bitmap.bmp', './img/reverse-non-palette.bmp');
-invertColors('./img/palette-bitmap.bmp', './img/invert-palette.bmp');
-// grayScale('./img/non-palette-bitmap.bmp', './img/gray-non-palette.bmp');
--- streamFile.js
+++ streamFile.js
@@ -3,7 +3,7 @@ var Transform = require('stream').Transform;
 var util = require('util');
 
 //async function to open the file and read just the header info
-function getHeader(filename, callback) {
+exports.getHeader = function getHeader(filename, callback) {
   var buffer = new Buffer(54);
   fs.open(filename, 'r', function(err, fd) {
     if (err) {
@@ -30,67 +30,65 @@ function getHeader(filename, callback) {
 
       callback(null, image);
     }
-  };
+  }
 };
 
 //Read the image file header, streams image if it is 24bpp
-function grayScale(readFile, writeFile) {
-  getHeader(readFile, function(err, image) {
+exports.grayScale = function grayScale(readFile, writeFile, callback) {
+  var writable = fs.createWriteStream(writeFile);
+  writable.on('finish', callback);
+  this.getHeader(readFile, function(err, image) {
     if (err) {
-      console.log('could not get header', err);
+      callback(err);
     }
     else if (image.depth === 24) {
       fs.createReadStream(readFile)
         .pipe(new GrayscaleStream())
-        .pipe(fs.createWriteStream(writeFile));
+        .pipe(writable);
     }
   });
-};
 
-util.inherits(GrayscaleStream, Transform);
+  util.inherits(GrayscaleStream, Transform);
 
-function GrayscaleStream() {
-  Transform.call(this);
-  this.offset = 0;
-  this.headerOverflow = false;
-  this.leftovers = null;
-}
+  function GrayscaleStream() {
+    Transform.call(this);
+    this.offset = 0;
+    this.headerOverflow = false;
+    this.leftovers = null;
+  }
 
-GrayscaleStream.prototype._transform = function (chunk, encoding, processed) {
-  var length = chunk.length;
-  if (this.offset < 54) { //check to see if past image start
-    if (54 - this.offset >= length) { //check to see if chunk contains more than header
-      this.push(chunk);
-      this.offset += length;
-    } else {
-      this.push(chunk.slice(this.offset, 54)); //push header down the pipe
-      this.offset = 54;
-      this.leftovers = chunk.slice(54); //store extra pixels
-      this.headerOverflow = true;
+  GrayscaleStream.prototype._transform = function (chunk, encoding, processed) {
+    var length = chunk.length;
+    if (this.offset < 54) { //check to see if past image start
+      if (54 - this.offset >= length) { //check to see if chunk contains more than header
+        this.push(chunk);
+        this.offset += length;
+      } else {
+        this.push(chunk.slice(this.offset, 54)); //push header down the pipe
+        this.offset = 54;
+        this.leftovers = chunk.slice(54); //store extra pixels
+        this.headerOverflow = true;
+      }
     }
-  }
-  if (this.offset >= 54) { //conditions for chunks that did not contain any header info
-    if (this.leftovers && !this.headerOverflow) {
-      this.leftovers = Buffer.concat([this.leftovers, chunk]);
-    } else if (!this.leftovers && !this.headerOverflow) {
-      this.leftovers = chunk;
+    if (this.offset >= 54) { //conditions for chunks that did not contain any header info
+      if (this.leftovers && !this.headerOverflow) {
+        this.leftovers = Buffer.concat([this.leftovers, chunk]);
+      } else if (!this.leftovers && !this.headerOverflow) {
+        this.leftovers = chunk;
+      }
     }
-  }
-  this.headerOverflow = false;
-  while (this.leftovers.length >= 3) {
-    var oldBlue = this.leftovers.readUInt8(0);
-    var oldGreen = this.leftovers.readUInt8(1);
-    var oldRed = this.leftovers.readUInt8(2);
-    var hue = (oldBlue + oldGreen + oldRed) / 3;
-    this.leftovers.writeUInt8(hue, 0);
-    this.leftovers.writeUInt8(hue, 1);
-    this.leftovers.writeUInt8(hue, 2);
-    this.push(this.leftovers.slice(0, 3));
-    this.leftovers = this.leftovers.slice(3);
-  }
-  processed();
+    this.headerOverflow = false;
+    while (this.leftovers.length >= 3) {
+      var oldBlue = this.leftovers.readUInt8(0);
+      var oldGreen = this.leftovers.readUInt8(1);
+      var oldRed = this.leftovers.readUInt8(2);
+      var hue = (oldBlue + oldGreen + oldRed) / 3;
+      this.leftovers.writeUInt8(hue, 0);
+      this.leftovers.writeUInt8(hue, 1);
+      this.leftovers.writeUInt8(hue, 2);
+      this.push(this.leftovers.slice(0, 3));
+      this.leftovers = this.leftovers.slice(3);
+    }
+    processed();
+  };
 };
-
-
-//command to be called
-grayScale('./img/non-palette-bitmap.bmp', './img/gray-non-palette.bmp');
